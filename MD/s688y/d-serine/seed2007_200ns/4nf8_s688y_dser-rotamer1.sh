d1dca6c36994a87ef03921eef19c5dfd53881c85
jak-chen <jake.chen@sydney.edu.au>
2021-06-18 14:05:25 +1000

    added s688y dser files

---
+++ MD/s688y/d-serine/seed2007_200ns/4nf8_s688y_dser-rotamer1.sh
@@ -0,0 +1,1 @@
+"${SCHRODINGER}/utilities/multisim" -JOBNAME 4nf8_s688y_dser-rotamer1 -HOST '<dummy-gpu-host>' -maxjob 1 -cpu 1 -m 4nf8_s688y_dser-rotamer1.msj -c 4nf8_s688y_dser-rotamer1.cfg -description 'Molecular Dynamics' 4nf8_s688y_dser-rotamer1.cms -mode umbrella -set 'stage[1].set_family.md.jlaunch_opt=["-gpu"]' -o 4nf8_s688y_dser-rotamer1-out.cms -lic DESMOND_GPGPU:16
